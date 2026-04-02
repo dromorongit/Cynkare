8cfa8d89d68bfac6b0c9a41c678869405a726c68
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-04-02 12:27:37 +0000

    Add subcategories property to staticCategories

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,9 +1,9 @@
 export const staticCategories = [
-  { id: '1', name: 'Body Lotions', slug: 'body-lotions', image: '' },
-  { id: '2', name: 'Bath Soaps', slug: 'bath-soaps', image: '' },
-  { id: '3', name: 'Face Creams & Cleansers', slug: 'face-creams-cleansers', image: '' },
-  { id: '4', name: 'Perfumes', slug: 'perfumes', image: '' },
-  { id: '5', name: 'Hair & Accessories', slug: 'hair-accessories', image: '' },
+  { id: '1', name: 'Body Lotions', slug: 'body-lotions', image: '', subcategories: [] },
+  { id: '2', name: 'Bath Soaps', slug: 'bath-soaps', image: '', subcategories: [] },
+  { id: '3', name: 'Face Creams & Cleansers', slug: 'face-creams-cleansers', image: '', subcategories: [] },
+  { id: '4', name: 'Perfumes', slug: 'perfumes', image: '', subcategories: [] },
+  { id: '5', name: 'Hair & Accessories', slug: 'hair-accessories', image: '', subcategories: [] },
 ];
 
 export const getCategoryBySlug = (slug: string) => {
