116e78518622e550c7f398310ab8dbb9185d8452
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-04-02 12:22:41 +0000

    Add image property to staticCategories type

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,9 +1,9 @@
 export const staticCategories = [
-  { id: '1', name: 'Body Lotions', slug: 'body-lotions' },
-  { id: '2', name: 'Bath Soaps', slug: 'bath-soaps' },
-  { id: '3', name: 'Face Creams & Cleansers', slug: 'face-creams-cleansers' },
-  { id: '4', name: 'Perfumes', slug: 'perfumes' },
-  { id: '5', name: 'Hair & Accessories', slug: 'hair-accessories' },
+  { id: '1', name: 'Body Lotions', slug: 'body-lotions', image: '' },
+  { id: '2', name: 'Bath Soaps', slug: 'bath-soaps', image: '' },
+  { id: '3', name: 'Face Creams & Cleansers', slug: 'face-creams-cleansers', image: '' },
+  { id: '4', name: 'Perfumes', slug: 'perfumes', image: '' },
+  { id: '5', name: 'Hair & Accessories', slug: 'hair-accessories', image: '' },
 ];
 
 export const getCategoryBySlug = (slug: string) => {
