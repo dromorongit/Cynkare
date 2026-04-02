94f972753deea105f78fa3e2ee79b31eb786b262
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-04-02 16:23:36 +0000

    Add background images to category sections

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,9 +1,39 @@
 export const staticCategories = [
-  { id: '1', name: 'Body Lotions', slug: 'body-lotions', image: '', subcategories: [] },
-  { id: '2', name: 'Bath Soaps', slug: 'bath-soaps', image: '', subcategories: [] },
-  { id: '3', name: 'Face Creams & Cleansers', slug: 'face-creams-cleansers', image: '', subcategories: [] },
-  { id: '4', name: 'Perfumes', slug: 'perfumes', image: '', subcategories: [] },
-  { id: '5', name: 'Hair & Accessories', slug: 'hair-accessories', image: '', subcategories: [] },
+  { 
+    id: '1', 
+    name: 'Body Lotions', 
+    slug: 'body-lotions', 
+    image: 'https://images.unsplash.com/photo-1556228720-195a672e8a03?w=1200&h=600&fit=crop', 
+    subcategories: [] 
+  },
+  { 
+    id: '2', 
+    name: 'Bath Soaps', 
+    slug: 'bath-soaps', 
+    image: 'https://images.unsplash.com/photo-1600857062241-98e5dba7f214?w=1200&h=600&fit=crop', 
+    subcategories: [] 
+  },
+  { 
+    id: '3', 
+    name: 'Face Creams & Cleansers', 
+    slug: 'face-creams-cleansers', 
+    image: 'https://images.unsplash.com/photo-1570194065650-d99fb4b38b19?w=1200&h=600&fit=crop', 
+    subcategories: [] 
+  },
+  { 
+    id: '4', 
+    name: 'Perfumes', 
+    slug: 'perfumes', 
+    image: 'https://images.unsplash.com/photo-1541643600914-78b084683601?w=1200&h=600&fit=crop', 
+    subcategories: [] 
+  },
+  { 
+    id: '5', 
+    name: 'Hair & Accessories', 
+    slug: 'hair-accessories', 
+    image: 'https://images.unsplash.com/photo-1527799820374-dcf8d9d4a388?w=1200&h=600&fit=crop', 
+    subcategories: [] 
+  },
 ];
 
 export const getCategoryBySlug = (slug: string) => {
